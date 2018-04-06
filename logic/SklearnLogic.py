f789e1483db5480561d08b7f2b51f608547d5953
PangQian <1835896410@qq.com>
2018-04-06 22:29:35 +0800

    文本聚类

--- logic/SklearnLogic.py
+++ logic/SklearnLogic.py
@@ -3,6 +3,7 @@ import codecs
 from logic import BaseLogic
 from database import UserArticle
 from sklearn import feature_extraction
+from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -32,55 +33,14 @@ class SklearnLogic(BaseLogic.BaseLogic):
     def getTFIDF(self, ucid):
         corpus = []  # 文档预料 空格连接
 
-        limit  = 10
+        limit  = 100
         offset = 0
         user_article = UserArticle.UserArticle()
         articles = user_article.getUserArticle(ucid, offset, limit)
 
         for article in articles:
+            print(article)
             corpus.append(article['content_split'])
-
-            # 将文本中的词语转换为词频矩阵 矩阵元素a[i][j] 表示j词在i类文本下的词频
-            vectorizer = CountVectorizer()
-
-            # 该类会统计每个词语的tf-idf权值
-            transformer = TfidfTransformer()
-
-            # 第一个fit_transform是计算tf-idf 第二个fit_transform是将文本转为词频矩阵
-            tfidf = transformer.fit_transform(vectorizer.fit_transform(corpus))
-
-            # 获取词袋模型中的所有词语
-            word = vectorizer.get_feature_names()
-
-            # 将tf-idf矩阵抽取出来，元素w[i][j]表示j词在i类文本中的tf-idf权重
-            weight = tfidf.toarray()
-
-            resName = "BaiduTfidf_Result.txt"
-            result  = codecs.open(resName, 'w', 'utf-8')
-            for j in range(len(word)):
-                result.write(word[j] + ' ')
-            result.write('\r\n\r\n')
-
-            # 打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重
-            for i in range(len(weight)):
-                print(u"-------这里输出第", i, u"类文本的词语tf-idf权重------")
-
-                for j in range(len(word)):
-                    result.write(str(weight[i][j]) + ' ')
-                result.write('\r\n\r\n')
-
-            result.close()
-
-
-    def test(self):
-        corpus = []  # 文档预料 空格连接
-
-        # 读取预料 一行预料为一个文档
-        for line in open('BaiduSpider_Result.txt', 'r').readlines():
-            print(line)
-            corpus.append(line.strip())
-        # print corpus
-        time.sleep(5)
 
         # 将文本中的词语转换为词频矩阵 矩阵元素a[i][j] 表示j词在i类文本下的词频
         vectorizer = CountVectorizer()
@@ -98,14 +58,14 @@ class SklearnLogic(BaseLogic.BaseLogic):
         weight = tfidf.toarray()
 
         resName = "BaiduTfidf_Result.txt"
-        result = codecs.open(resName, 'w', 'utf-8')
+        result  = codecs.open(resName, 'w', 'utf-8')
         for j in range(len(word)):
             result.write(word[j] + ' ')
         result.write('\r\n\r\n')
 
         # 打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重
         for i in range(len(weight)):
-            print (u"-------这里输出第", i, u"类文本的词语tf-idf权重------")
+            print(u"-------这里输出第", i, u"类文本的词语tf-idf权重------")
 
             for j in range(len(word)):
                 result.write(str(weight[i][j]) + ' ')
@@ -113,7 +73,72 @@ class SklearnLogic(BaseLogic.BaseLogic):
 
         result.close()
 
+        return weight
+
+    # K-means聚类
+    def getKmeans(self, weight):
+        print('Start Kmeans:')
+        clf = KMeans(n_clusters = 20)
+        s   = clf.fit(weight)
+        print(s)
+
+        # 20个中心点
+        print(clf.cluster_centers_)
+
+        # 每个样本所属的簇
+        print(clf.labels_)
+        i = 1
+        while i <= len(clf.labels_):
+            print(i, clf.labels_[i - 1])
+
+            i = i + 1
+
+        # 用来评估簇的个数是否合适，距离越小说明簇分的越好，选取临界点的簇个数
+        print(clf.inertia_)
+
+    def test(self):
+            corpus = []  # 文档预料 空格连接
+
+            # 读取预料 一行预料为一个文档
+            for line in open('BaiduSpider_Result.txt', 'r').readlines():
+                print(line)
+                corpus.append(line.strip())
+            # print corpus
+            time.sleep(5)
+
+            # 将文本中的词语转换为词频矩阵 矩阵元素a[i][j] 表示j词在i类文本下的词频
+            vectorizer = CountVectorizer()
+
+            # 该类会统计每个词语的tf-idf权值
+            transformer = TfidfTransformer()
+
+            # 第一个fit_transform是计算tf-idf 第二个fit_transform是将文本转为词频矩阵
+            tfidf = transformer.fit_transform(vectorizer.fit_transform(corpus))
+
+            # 获取词袋模型中的所有词语
+            word = vectorizer.get_feature_names()
+
+            # 将tf-idf矩阵抽取出来，元素w[i][j]表示j词在i类文本中的tf-idf权重
+            weight = tfidf.toarray()
+
+            resName = "BaiduTfidf_Result.txt"
+            result = codecs.open(resName, 'w', 'utf-8')
+            for j in range(len(word)):
+                result.write(word[j] + ' ')
+            result.write('\r\n\r\n')
+
+            # 打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重
+            for i in range(len(weight)):
+                print (u"-------这里输出第", i, u"类文本的词语tf-idf权重------")
+
+                for j in range(len(word)):
+                    result.write(str(weight[i][j]) + ' ')
+                result.write('\r\n\r\n')
+
+            result.close()
+
 
 if __name__ == '__main__':
     a = SklearnLogic()
-    a.getTFIDF('1768305123')
+    weight = a.getTFIDF('1768305123')
+    a.getKmeans(weight)
